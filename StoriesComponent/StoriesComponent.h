7dc283dbfae5166e13c9c953c09d12a606f1c4fb
Pedro Seruca <pedroseruca@gmail.com>
2021-05-13 23:18:44 +0100

    Initial commit. Setup Project

---
+++ StoriesComponent/StoriesComponent.h
@@ -0,0 +1,18 @@
+//
+//  StoriesComponent.h
+//  StoriesComponent
+//
+//  Created by Pedro Seruca on 13/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StoriesComponent.
+FOUNDATION_EXPORT double StoriesComponentVersionNumber;
+
+//! Project version string for StoriesComponent.
+FOUNDATION_EXPORT const unsigned char StoriesComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoriesComponent/PublicHeader.h>
+
+
